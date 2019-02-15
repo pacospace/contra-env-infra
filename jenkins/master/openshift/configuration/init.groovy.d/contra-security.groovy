9acd54c8b22108c217c77ee2ff01b8f434ce6b0a
Subin <subin@apache.org>
2019-02-15 11:54:49 -0500

    fix tf jenkins

--- jenkins/master/openshift/configuration/init.groovy.d/contra-security.groovy
+++ jenkins/master/openshift/configuration/init.groovy.d/contra-security.groovy
@@ -9,25 +9,25 @@ import jenkins.model.*
 import jenkins.security.s2m.*
 
 
-def logger = Logger.getLogger("")
-logger.info("Disabling CLI over remoting")
-jenkins.CLI.get().setEnabled(false);
-logger.info("Enable Slave -> Master Access Control")
-Jenkins.instance.injector.getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
-// Set global read permission
-def strategy = Jenkins.instance.getAuthorizationStrategy()
-strategy.add(hudson.model.Hudson.READ,'anonymous')
-// users with URL will be presented with login screen
-strategy.add(hudson.model.Item.DISCOVER,'anonymous')
-Jenkins.instance.setAuthorizationStrategy(strategy)
-// Set Markup Formatter to Safe HTML so PR hyperlinks work
-Jenkins.instance.setMarkupFormatter(new RawHtmlMarkupFormatter(false))
+// def logger = Logger.getLogger("")
+// logger.info("Disabling CLI over remoting")
+// jenkins.CLI.get().setEnabled(false);
+// logger.info("Enable Slave -> Master Access Control")
+// Jenkins.instance.injector.getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
+// // Set global read permission
+// def strategy = Jenkins.instance.getAuthorizationStrategy()
+// strategy.add(hudson.model.Hudson.READ,'anonymous')
+// // users with URL will be presented with login screen
+// strategy.add(hudson.model.Item.DISCOVER,'anonymous')
+// Jenkins.instance.setAuthorizationStrategy(strategy)
+// // Set Markup Formatter to Safe HTML so PR hyperlinks work
+// Jenkins.instance.setMarkupFormatter(new RawHtmlMarkupFormatter(false))
 
-logger.info("Enabling CSRF Protection")
-Jenkins.instance.setCrumbIssuer(new DefaultCrumbIssuer(true))
+// logger.info("Enabling CSRF Protection")
+// Jenkins.instance.setCrumbIssuer(new DefaultCrumbIssuer(true))
 
-logger.info("Disabling deprecated agent protocols (only JNLP4 is enabled)")
-Set<String> agentProtocolsList = ['JNLP4-connect']
-Jenkins.instance.setAgentProtocols(agentProtocolsList)
+// logger.info("Disabling deprecated agent protocols (only JNLP4 is enabled)")
+// Set<String> agentProtocolsList = ['JNLP4-connect']
+// Jenkins.instance.setAgentProtocols(agentProtocolsList)
 
-Jenkins.instance.save()
+// Jenkins.instance.save()
